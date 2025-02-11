263b3ff7122d530cdbdd3325da6c93e251784144
PieFlavr <lucasadampinto@gmail.com>
2025-02-11 11:49:37 -0800

    Generate empty __init__ and __main__ files.

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+"""
+__init__.py
+
+Description: This script initializes and runs the main application for the CPE471 Project 1.
+Author: Lucas Pinto
+Date: February 11, 2025
+
+"""
---
+++ src/__main__.py
@@ -0,0 +1,16 @@
+"""
+__main__.py
+
+Description: This script initializes and runs the main application for the CPE471 Project 1.
+Author: Lucas Pinto
+Date: February 11, 2025
+
+"""
+
+from __init__ import * # Import everything from the __init__.py file
+
+if __name__ == "__main__":
+    
+    
+
+    pass
