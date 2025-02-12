87ae89508332a443d000a0096fb9493ea539ec36
PieFlavr <lucasadampinto@gmail.com>
2025-02-11 17:30:07 -0800

    Refactor GridWorld class methods for better encapsulation and add reward system

--- src/grid_world.py
+++ src/grid_world.py
@@ -1,3 +1,21 @@
+"""
+__init__.py
+
+Description: Main program entry point. The primary loop for the agent is here.
+Author: Lucas Pinto
+Date: February 10, 2025
+
+Modules:
+    numpy
+    matplotlib
+    functions
+
+Functions:
+    helloWorld
+
+Usage:
+    python main.py
+"""
 import numpy as np
 from typing import Tuple
 
@@ -19,7 +37,7 @@ class GridWorld:
         self._grid = np.zeros(grid_dim)
         self._grid[agent.position.x, agent.position.y] = 2 # Represents the agent as a 2 in the grid
 
-    def move_agent(self, action: str = None) -> bool:
+    def _move_agent(self, action: str = None) -> bool:
         """
         Move the agent in the specified direction and update the grid.
 
@@ -33,7 +51,7 @@ class GridWorld:
         self._agent.move(action, self._grid_dim)
         self._grid[self._agent.position.x, self._agent.position.y] = 2
     
-    def reset_agent(self):
+    def _reset_agent(self):
         """
         Reset the agent's position to a random position within the grid excluding the goal.
         """
@@ -44,7 +62,33 @@ class GridWorld:
                 break
         self._grid[self._agent.position.x, self._agent.position.y] = 2
     
-    def get_agent_position(self):
+    def _is_goal_reached(self) -> bool:
+        """
+        Check if the agent has reached the goal.
+
+        Returns:
+            bool: True if the agent has reached the goal, False otherwise.
+        """
+        return self._agent.position == self._goal
+    
+    def _get_reward(self, move_successful: bool = None):
+        """
+        Get the reward based on the agent's action and position.
+
+        Args:
+            successful (bool, optional): Indicates if the agent's move was successful. Defaults to True.
+
+        Returns:
+            float: The reward value based on the agent's action and position.
+        """
+        if self._is_goal_reached():
+            return 10 # If the agent has reached the goal, return a reward of 10
+        elif move_successful:
+            return -0.1 # If the agent has moved successfully, return a reward of -0.1
+        else: 
+            return -1 # If the agent has hit a wall/invalid move, return a reward of -1
+
+    def _get_agent_position(self):
         """
         Get the current position of the agent.
 
@@ -53,3 +97,27 @@ class GridWorld:
         """
         return tuple(self._agent.position.x, self._agent.position.y)
     
+    def step_agent(self, action: str) -> Tuple[float, bool]:
+        """
+        Perform a step in the environment by moving the agent.
+
+        Args:
+            action (str): The direction to move the agent. Can be 'up', 'down', 'left', or 'right'.
+
+        Returns:
+            Tuple[float, bool]: A tuple containing the reward for the action and a boolean indicating if the goal has been reached.
+        """
+        move_successful = self._move_agent(action)
+        reward = self._get_reward(move_successful)
+        done = self._is_goal_reached()
+
+        return reward, done
+        
+    def get_state(self) -> Tuple[np.ndarray[int], Tuple[int,int]]:
+        """
+        Get the current state of the grid and the agent's position.
+
+        Returns:
+            tuple: A tuple containing the grid and the agent's current position.
+        """
+        return self._grid, self.get_agent_position()
