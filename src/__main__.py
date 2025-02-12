b931682be966c0dfe9fe730b76ee7d4a78b4ea1e
PieFlavr <lucasadampinto@gmail.com>
2025-02-11 19:10:29 -0800

    Add main function to run Q-learning training in GridWorld environment

--- src/__main__.py
+++ src/__main__.py
@@ -9,8 +9,42 @@ Date: February 11, 2025
 
 from __init__ import * # Import everything from the __init__.py file
 
-if __name__ == "__main__":
-    
-    
+def main():
+    """
+    Main function to run the application.
+    """
 
+    print("Hello, World!")
+
+    if __name__ == "__main__":
+        print()
+        grid_length = 5
+        grid_width = 5
+
+        episodes = 1000
+        alpha = 0.15
+        gamma = 0.95
+        epsilon = 0.1
+
+        actions = {'up': 0, 'down': 1, 'left': 2, 'right': 3}
+        environment = GridWorld((grid_length, grid_width), None, (grid_length-1, grid_width-1))
+        q_table = np.zeros((grid_length, grid_width, len(actions)), dtype = float) # Initialize Q-table with zeros
+        
+        training_data = []
+
+        print("Training Q-Learning agent...")
+        
+        for episode in range(episodes):
+            print(f"Episode {episode+1}/{episodes}")
+            training_data.append(Q_learning_episode(grid_world=environment, 
+                            agent=None, 
+                            actions=actions,
+                            q_table=q_table,
+                            selection_function=epsilon_greedy_selection,
+                            function_args={'q_table': q_table, 'epsilon': epsilon},
+                            alpha=alpha, 
+                            gamma=gamma))
+        print("Q-Learning Training completed.")
     pass
+
+main()
